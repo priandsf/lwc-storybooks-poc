f8b909e2dea4e2bff59b102b5bc9bb09e97d70a4
Philippe Riand <priand@salesforce.com>
2020-06-05 10:48:17 -0400

    Moved to the latest LWC - WebPack -  Global components

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,9 @@
 import { configure } from '@storybook/html';
 import '!style-loader!css-loader!@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css';
 
+// PHIL's test
+//import '@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.css';
+
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
 function loadStories() {
